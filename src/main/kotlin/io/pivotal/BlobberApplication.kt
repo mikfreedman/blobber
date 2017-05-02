a238d735c1e18c4c224d0b4dff078ddd6a955396
Mik Freedman <pair+mik@pivotal.io>
2017-05-02 13:31:30 -0600

    remove unused imports

--- src/main/kotlin/io/pivotal/BlobberApplication.kt
+++ src/main/kotlin/io/pivotal/BlobberApplication.kt
@@ -1,10 +1,7 @@
 package io.pivotal
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
 class BlobberApplication
